52648dc72fce517df3bc3850c2436e1ea554d233
zhaoqiangwei <zhaoqiangwei20022@163.com>
2020-03-03 02:58:21 +0800

    尝试添加requires和install_requires

--- README.txt
+++ README.txt
@@ -1,2 +1,3 @@
 # 这是我尝试进行构建的第一个python包
 
+# 配置文件的优先级 setup.py < MANIFEST.in < setup.cfg
--- setup.py
+++ setup.py
@@ -3,31 +3,63 @@ from setuptools import setup, find_packages
 setup(
     name="hello_python",
     version="1.0",
-    description="安装hello_python的学习测试项目",
+    description="安装hello_python并打包上传pypi",
     author="zhaoqiangwei",
-    author_email="zhaoqiangwei@qq.com",
+    author_email="demo@qq.com",
     url="https://www.baidu.com",
-    # python解释器，sitepackages里面的最顶层文件
+    license="MIT",
+    keywords=["hello_python", "hello_python_demo"],
 
-    # python解释器，sitepackages里面的模块
+    # 1.python解释器里的顶级文件夹映射进来的对应目录
     package_dir={
         '': 'src',
     },
-    # packages=["module1","module2", "module1.scripts", ],
+    # 2.python解释器，site-packages里面顶级包(package)
     packages = find_packages(where="src", exclude=["module2"], include=["module1", "module1.*"]),
+    # 3.python解释器，site-packages里面顶级文件
     py_modules=["hello_python1_top1", "hello_python1_top2"],
 
-    # python解释器，scripts的文件
+    # 4.python解释器，Scripts目录（在site-packageages外面）
     scripts = ["src/scripts/hello_python_script1.py", ],
 
+    # 5.python解释器，c语言相关构建用
     ext_modules=[],
     ext_package=[],
-    # python解释器，最外层的文件,这个里面无法用正则表达式
+
+    # 6.python解释器，最外层的文件,这个里面无法用正则表达式
     data_files=[
         ('hello_python_bm', ['src/data/bm/b1.txt', 'src/data/bm/b2.txt']),
         ('hello_python_cfg', ['src/data/cfg/data.cfg']),
         ('hello_python_sh', ['src/data/sh/shell1.sh'])
                 ],
-    requires = ["request", "six"],
+
+    # 7.python解释器，对应的是MANIFEST.in(但是好像也不用设置)
+    include_package_data=True,
+
+    requires = [
+        'request',
+        'six',
+        'redis',
+        'pytest',
+    ],
+    install_requires=[
+        'enum34;python_version<"3.4"',
+        'pywin32 >= 1.0;platform_system=="Windows"',
+        'request',
+        'six',
+        'redis >= 2.10.5',
+        'pytest',
+    ],
+    classifiers = [
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 3',
+          'Operating System :: Unix',
+          'Operating System :: Microsoft :: Windows',
+          'Operating System :: MacOS',
+          'Intended Audience :: Science/Research',
+          'Topic :: Scientific/Engineering',
+          'Topic :: Software Development'
+      ],
 
 )
